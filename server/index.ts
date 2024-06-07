ddae770dc45bb1bab3cc29fa66eac4c61480730b
Andhika_RK <andhikarestu873@gmail.com>
2024-06-07 22:47:15 +0700

    setup prisma and index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+const st = new Date().getTime();
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+
+const app = new Hono({ strict: false });
+app.use(logger());
+app.get('/', (c) =>
+	c.json({
+		success: true,
+		payload: null,
+		message: 'Welcome to Hono',
+	})
+);
+
+const en = new Date().getTime();
+
+console.log(`${en - st} ms to start the server`);
+export default {
+	port: Bun.env.PORT || 8000,
+	fetch: app.fetch,
+};
