57008db8820262f86b285b283e084d779bf61c2e
sennes <109758797+nsennes@users.noreply.github.com>
2024-06-07 16:48:03 +0700

    Setting up front end

---
+++ safeschool/lib/Utilities/colors_use.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsUse {
+  static const primaryColor = Color(0xff1F8C75);
+  static const secondaryColor = Color(0xffDCE0D9);
+  static const accentColor = Color(0xff0C1104);
+  static const backgroundColor = Color(0xffF3F5F1);
+}
