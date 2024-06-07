4d6729c22a44d479535ec727eaf2061e3c27fb87
sennes <109758797+nsennes@users.noreply.github.com>
2024-06-07 17:32:00 +0700

    set the colors and fonts

---
+++ safeschool/lib/Utilities/text_use.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:safeschool/Utilities/colors_use.dart';
+
+class TextUse {
+  // TextStyle
+}
--- safeschool/lib/main.dart
+++ safeschool/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:safeschool/Utilities/colors_use.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,15 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SafeSchool',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.green,
-          brightness: Brightness.light,
-          primary: Colors.green,
-          onPrimary: Colors.white,
-          secondary: Colors.teal,
-          onSecondary: Colors.black,
+            seedColor: ColorsUse.primaryColor,
+            brightness: Brightness.light,
+            primary: ColorsUse.primaryColor,
+            onPrimary: ColorsUse.secondaryColor,
+            secondary: ColorsUse.secondaryColor,
+            onSecondary: ColorsUse.accentColor,
+            onSurface: ColorsUse.backgroundColor),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: ColorsUse.primaryColor,
         ),
+        fontFamily: ("Rubik"),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -29,15 +35,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -72,7 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
