dca2b43746e61de3f6e976d1fe83154e768af63d
sennes <109758797+nsennes@users.noreply.github.com>
2024-06-13 20:12:51 +0700

    articles :100:

--- safeschool/lib/Widgets/article_card.dart
+++ safeschool/lib/Widgets/article_card.dart
@@ -1,10 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:safeschool/Utilities/colors_use.dart';
+import 'package:safeschool/Utilities/text_use.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ArticleCard extends StatelessWidget {
-  const ArticleCard({super.key});
+  final String title;
+  final String author;
+  final String img;
+  final String url;
+  final EdgeInsetsGeometry? titlePadding;
+  final CrossAxisAlignment? titleAlignment;
+
+  const ArticleCard({
+    super.key,
+    required this.title,
+    required this.author,
+    required this.img,
+    required this.url,
+    this.titlePadding,
+    this.titleAlignment,
+  });
+
+  Future<void> _launchUrl() async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri)) {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: _launchUrl,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
+        child: Container(
+          height: 200,
+          width: 370,
+          decoration: BoxDecoration(
+            color: ColorsUse.secondaryColor,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.25),
+                spreadRadius: -2,
+                blurRadius: 6,
+                offset: const Offset(0, 8),
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0, left: 12, bottom: 20),
+                child: SizedBox(
+                  height: 180,
+                  width: 190,
+                  child: Image.asset(img),
+                ),
+              ),
+              Padding(
+                padding: titlePadding ??
+                    const EdgeInsets.only(top: 60.0, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment:
+                      titleAlignment ?? CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextUse.heading_2().merge(
+                          const TextStyle(color: ColorsUse.primaryColor)),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      author,
+                      style: TextUse.heading_3().merge(const TextStyle(
+                        fontSize: 12,
+                        color: ColorsUse.accentColor,
+                      )),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- safeschool/lib/pages/Resources/articles.dart
+++ safeschool/lib/pages/Resources/articles.dart
@@ -1,59 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:safeschool/Utilities/colors_use.dart';
-import 'package:safeschool/Utilities/text_use.dart';
+import 'package:safeschool/Widgets/article_card.dart';
 
 class Articles extends StatelessWidget {
   const Articles({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 20.0, left: 20, right: 20),
-        child: Container(
-          height: 200,
-          width: 370,
-          decoration: BoxDecoration(
-            color: ColorsUse.secondaryColor,
-            borderRadius: BorderRadius.circular(15),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.25),
-                spreadRadius: -2,
-                blurRadius: 6,
-                offset: const Offset(0, 8),
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0, left: 12, bottom: 20),
-                child: SizedBox(
-                  height: 150,
-                  width: 130,
-                  child: Image.asset("assets/images/article_1.png"),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 70.0, left: 20, right: 20),
-                child: Column(
-                  children: [
-                    Text(
-                      "What is Bullying?",
-                      style: TextUse.heading_2().merge(
-                          const TextStyle(color: ColorsUse.primaryColor)),
-                    ),
-                    Text(
-                      "StopBullying.gov",
-                      style: TextUse.heading_3().merge(const TextStyle(
-                          fontSize: 12, color: ColorsUse.accentColor)),
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
+    return const Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ArticleCard(
+              title: "What is\n bullying?",
+              author: "StopBullying.gov",
+              img: "assets/images/a1.jpg",
+              url: "https://www.stopbullying.gov/what-is-bullying",
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ArticleCard(
+              title: "How to Deal\nWith Bullies",
+              author: "KidsHealth",
+              img: "assets/images/a2.jpg",
+              url: "https://kidshealth.org/en/kids/bullies.html",
+              titlePadding: EdgeInsets.only(top: 60.0, left: 15),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ArticleCard(
+              title: "How\nBullying\nHotlines\nWork",
+              author: "Verywell Family",
+              img: "assets/images/a3.png",
+              url:
+                  "https://www.verywellfamily.com/how-bullying-hotlines-work-4589992",
+              titlePadding: EdgeInsets.only(top: 30, left: 35),
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
         ),
       ),
     );
